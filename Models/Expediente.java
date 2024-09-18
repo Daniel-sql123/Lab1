a63103767647d747740d0ed4af043746acf8cbcc
Daniel-sql123 <dadelgadoal@est.utn.ac.cr>
2024-09-18 15:59:56 -0600

    Clase persona
    Se crea la clase corregida

--- Models/Expediente.java
+++ Models/Expediente.java
@@ -4,10 +4,46 @@
  */
 package Models;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 /**
  *
  * @author Usuario
  */
 public class Expediente {
+    
+    private String nombre;
+    private LocalDate edad;
+    private CitasListas<Citas> historial;
+
+    
+    public Expediente(String nombrePaciente, LocalDate edadPaciente) {
+        this.nombre = nombrePaciente;
+        this.edad = edadPaciente;
+        this.historial = new ArrayList<>();
+    }
+    
+     public void agregarCita(Citas cita) {
+        historial.add(cita);
+    }
+    
+    
+    
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getEdad() {
+        return edad;
+    }
+
+    public <any> getHistorial() {
+        return historial;
+    }
+    
+    
     
 }
--- Models/Persona.java
+++ Models/Persona.java
@@ -4,10 +4,63 @@
  */
 package Models;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Usuario
  */
 public class Persona {
+    private int id;
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private String telefono;
+    private String correo;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Persona(int id, String nombre, LocalDate fechaNacimiento, String telefono, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+    
+    public Persona(){
+        this(0,"",LocalDate.now(),"","");
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", nombre=" + nombre + ", fechaNacimiento=" + fechaNacimiento + ", telefono=" + telefono + ", correo=" + correo + '}';
+    }
+    
     
 }
